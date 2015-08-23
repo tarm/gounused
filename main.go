a8dce7000b80ef8dc385b216f0aaba873b7a8f51
Tarmigan Casebolt <tarmigan+git@gmail.com>
2015-08-23 14:49:35 -0700

    Working!

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"go/parser"
 	"go/token"
 	"log"
+	"os"
 
 	_ "golang.org/x/tools/go/gcimporter"
 	"golang.org/x/tools/go/loader"
@@ -51,7 +52,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	ssaprog := ssautil.CreateProgram(prog, 0)
+	ssaprog := ssautil.CreateProgram(prog, ssa.GlobalDebug)
 	ssaprog.BuildAll()
 	pkgs := ssaprog.AllPackages()
 	for _, p := range pkgs {
@@ -63,9 +64,47 @@ func main() {
 			for _, inst := range f.Blocks[0].Instrs {
 				if v, ok := inst.(ssa.Value); ok {
 					fmt.Println(v.Name())
-					fmt.Println("  (inst): ", inst)
-					fmt.Println("  (value): ", v.String(), v.Type())
-					fmt.Println("  (referrers): ", v.Referrers())
+					fmt.Printf("  (type)\t%v\n", v.Type())
+					fmt.Printf("  (inst)\t%v\n", inst)
+					fmt.Printf("  (referrers)\t%v\n", v.Referrers())
+					fmt.Printf("  (pos)\t%v\n", v.Pos())
+					fmt.Printf("  (debug)\t%#v\n", v)
+				}
+			}
+		}
+	}
+	fmt.Println()
+
+	// The real meat of things...
+	// Create a mapping from Defs to ssa.Values
+	// Make sure that each
+	for _, p := range pkgs {
+		if p.Object.Name() == "main" {
+			f := p.Func("main")
+			if f == nil {
+				log.Fatal("Could not find function")
+			}
+			for expr, object := range info.Uses {
+				if _, ok := object.(*types.Var); !ok {
+					continue
+				}
+				value, _ := f.ValueForExpr(expr)
+				fmt.Printf("%v %v: %v      %v\n", fset.Position(expr.Pos()), expr, object, value)
+				if value == nil {
+					continue
+				}
+				refs := value.Referrers()
+				if refs == nil {
+					continue
+				}
+				fmt.Printf("   (refs) %v\n", refs)
+				hasRef := false
+				for _, r := range *refs {
+					_, ok := r.(*ssa.DebugRef)
+					hasRef = hasRef || !ok
+				}
+				if !hasRef {
+					fmt.Fprintf(os.Stderr, "\nUnused assignment for %q %v\n\n", expr, fset.Position(expr.Pos()))
 				}
 			}
 		}
